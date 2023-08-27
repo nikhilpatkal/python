447f811779ac59cbc14fa7541d17794168d5ce05
nikhil patkal <110008815+nikhilpatkal@users.noreply.github.com>
2023-08-27 22:33:43 +0530

    Update  to some sentence Tip.py

--- Tip.py
+++ Tip.py
@@ -1,7 +1,7 @@
 
 amount=int(input("enter Bill amount"))
 Tip=int(input("how many tip you want"))
-person=int(input("how many person:"))
+person=int(input("how many person in group:"))
 Tip=(amount*Tip)/100
 Tip=amount+Tip
 Tip=Tip/person
